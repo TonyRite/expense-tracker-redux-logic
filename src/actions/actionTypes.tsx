0b7792c54d33bf9a56c02a65d06b643a60d2be7f
lioswin <lioswinrite@gmail.com>
2023-03-14 12:05:01 +0300

    action types

--- src/actions/actionTypes.tsx
+++ src/actions/actionTypes.tsx
@@ -1,10 +1,12 @@
-export function deleteTransaction(id){
+import { Transaction } from "../components/Balance";
+
+export function deleteTransaction(id:number){
         return {
             type:'DELETE_TRANSACTION',
             payload:id
         };
     }
-export function addTransaction(transaction){
+export function addTransaction(transaction: Transaction){
     return {
         type:'ADD_TRANSACTION',
         payload:transaction,
